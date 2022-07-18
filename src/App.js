b9aba4cf15ed2305afd88c83358dd13a1c57f696
Dawid Leszczynski <dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.comgit config --global user.email dawid.leszczynski1297@gmail.com>
2022-07-18 15:32:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">dsajdasjd da'kd daaSDa</div>;
+  return <div className="App">dsajdasasdasdasdasdjd da'kd daaSDa</div>;
 }
 
 export default App;
